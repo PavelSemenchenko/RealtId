ae36af34e3a68e6b0ed8b420230248195678edaf
PavelSemenchenko <imagemakerspa@icloud.com>
2025-02-25 21:16:25 +0200

    Initial Commit

---
+++ RealtId/RealtIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealtIdApp.swift
+//  RealtId
+//
+//  Created by Pavel Semenchenko on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RealtIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
